65a69c7043e28d13fc7a7a789af0fc8c4bd3c899
EC2 Default User <ec2-user@ip-172-31-17-149.ec2.internal>
2019-10-16 19:55:14 +0000

    Cart done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+ # get 'cart/index'
+
   resources :items
   root 'static_pages#home'
 
@@ -11,6 +13,26 @@ Rails.application.routes.draw do
   get '/login' => 'user#login'
   
     get '/logout' => 'user#logout'
+    
+    
+    
+    
+    
+    # This looks like what we did for the add item ...... Muuhhhhh yes Liam you are right......
+    
+    
+get '/remove/:id' => 'cart#remove'
+
+
+get '/reduce/:id' => 'cart#reduce'
+
+
+get '/cart/clearcart' => 'cart#clearcart'
+
+get '/cart' => 'cart#index'
+
+get '/cart/:id' => 'cart#add'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
